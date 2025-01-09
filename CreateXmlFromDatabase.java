4623c99adaa37952c754d599144b393b01ee6b9d
Monu.Alam <132586354+MonuAlam@users.noreply.github.com>
2025-01-09 10:32:01 +0530

    Update CreateXmlFromDatabase.java

--- CreateXmlFromDatabase.java
+++ CreateXmlFromDatabase.java
@@ -3,7 +3,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import javax.xml.transform.OutputKeys;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
